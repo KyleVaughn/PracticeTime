79505e67df20f427689a4a6dc6f958ea3eb15ec9
kyle <kcvaughn@umich.edu>
2020-08-25 19:02:13 -0400

    working

---
+++ initialize.py
@@ -0,0 +1,119 @@
+import gmsh
+import logging
+import sys
+
+# Filters messages of severity less than argument
+class LessThanFilter(logging.Filter):
+    def __init__(self, exclusive_maximum, name=""):
+        super(LessThanFilter, self).__init__(name)
+        self.max_level = exclusive_maximum
+
+    def filter(self, record):
+        #non-zero return means we log this message
+        return 1 if record.levelno < self.max_level else 0
+
+# Logging formatter to add colors to messages using ANSI
+class CustomFormatter(logging.Formatter):
+
+    def __init__(self, option=None):
+        self.option = option
+
+        purple = "\x1b[35;10m"
+        green = "\x1b[32;10m"
+        default = "\x1b[39;10m"
+        yellow = "\x1b[33;10m"
+        red = "\x1b[31;10m"
+        bold_red = "\x1b[31;1m"
+        reset = "\x1b[0m"
+                                                                                             
+        fmt =      "%(asctime)s %(levelname)-10s: %(name)s - %(message)s"
+        debugFmt = "%(asctime)s %(levelname)-10s: %(name)s - (line: %(lineno)d) %(message)s"
+
+        if option == 'debug':
+            theFormat = debugFmt
+        else:
+            theFormat = fmt
+
+        self.FORMATS = {
+            logging.DEBUG:    green    + theFormat + reset,
+            logging.INFO:     default  + theFormat + reset,
+            logging.WARNING:  purple   + theFormat + reset,
+            logging.ERROR:    bold_red + theFormat + reset,
+            logging.CRITICAL: bold_red + theFormat + reset
+        }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt, datefmt='%H:%M:%S')
+        return formatter.format(record)
+
+# Initialize mocmg logger and gmsh with desired level of output.
+# The levels are as follows:
+#   silent  : For gmsh only. Don't output anything from gmsh
+#   error   : Display error messages only
+#   warning : Display error and warning messages
+#   info    : Display error, warning, and info messages
+#   debug   : Display error, warning, info, and debug messages
+#
+# Inputs:
+#   mocmgOption: One of the levels above. String
+#   gmshOption: One of the leves above. String
+#   color: Option to color the output of log messages from mocmg. True or False
+def initialize(Option=None, color=True):
+    # mocmg
+    if Option == 'debug':
+        Verbosity = logging.DEBUG
+    elif Option == 'warning':
+        Verbosity = logging.WARNING
+    elif Option == 'error':
+        Verbosity = logging.ERROR
+    elif Option == 'silent':
+        Verbosity = 99
+    else:
+        Verbosity = logging.INFO
+
+    # Get the root logger
+    logger = logging.getLogger()
+    # Have to set the root logger level, it defaults to logging.WARNING
+    logger.setLevel(logging.NOTSET)
+    # Format stdout and stderr based upon color and debug mode
+    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-10s: %(name)s - %(message)s', 
+            datefmt='%H:%M:%S')
+    debugFormatter = logging.Formatter(fmt='%(asctime)s %(levelname)-10s: %(name)s - (line: %(lineno)d) %(message)s', 
+            datefmt='%H:%M:%S')
+
+    # Stdout
+    logging_handler_out = logging.StreamHandler(sys.stdout)
+    logging_handler_out.setLevel(Verbosity)
+    logging_handler_out.addFilter(LessThanFilter(logging.WARNING))
+
+    # If stdout is terminal, color if desired. Otherwise, don't color.
+    if color and sys.stdout.isatty():
+        if Option == 'debug':
+            logging_handler_out.setFormatter(CustomFormatter(option='debug'))
+        else:
+            logging_handler_out.setFormatter(CustomFormatter())
+    else:
+        if Option == 'debug':
+            logging_handler_out.setFormatter(debugFormatter)
+        else:
+            logging_handler_out.setFormatter(formatter)
+    logger.addHandler(logging_handler_out)
+
+    # Stderr
+    logging_handler_err = logging.StreamHandler(sys.stderr)
+    lvl = max(logging.WARNING, Verbosity)
+    logging_handler_err.setLevel(lvl)
+
+    # If stderr is terminal, color if desired. Otherwise, don't color.
+    if color and sys.stderr.isatty():
+        if Option == 'debug':
+            logging_handler_err.setFormatter(CustomFormatter(option='debug'))
+        else:
+            logging_handler_err.setFormatter(CustomFormatter())
+    else:
+        if Option == 'debug':
+            logging_handler_err.setFormatter(debugFormatter)
+        else:
+            logging_handler_err.setFormatter(formatter)
+    logger.addHandler(logging_handler_err)
--- schedule.py
+++ schedule.py
@@ -1,5 +1,17 @@
+import bisect
+from initialize import initialize
+from itertools import combinations
+import logging
 import numpy as np
 from person import Person
+
+# Assumed that time increment between times is 30 min
+
+nTimesToStore = 50
+dayOfInterest = 'Monday'
+nPracticeInc = 6
+nToPracticeInc = 1
+nFromPracticeInc = 2
 
 file_dir = './tsv/'
 files = [
@@ -11,9 +23,96 @@ files = [
         'Sahil.tsv',
         ]
 
+module_log = logging.getLogger(__name__)
+
+initialize()
+
 # Import data
+module_log.info('Importing info')
 people = []
 for f in files:
     people.append(Person.initFromFile(file_dir + f))
+
+# Make sure days and times are the same and all avail entries valid
+days = people[0].days
+times = people[0].times
+for i, p in enumerate(people):
+    if sorted(p.days) != sorted(days):
+        module_log.error(f'Not all days the same. See file {i}.')
+    if sorted(p.times) != sorted(times):
+        module_log.error(f'Not all times the same. See file {i}.')
+    for a in p.avail:
+        for x in a:
+            isclose = np.isclose(x,0) or np.isclose(x,1) or np.isclose(x,2)
+            if not isclose:
+                module_log.error(f'Not all avail values valid in file {i}.')
+
+# Add availability restrictions
+nTimes = len(people[0].avail[0])
 for p in people:
-    print(p.name)
+    for a in p.avail:
+        # Adjust for getting to and from practice
+        for i in range(nTimes-nToPracticeInc,0,-1):
+            if np.isclose(a[i],2):
+                for j in range(nToPracticeInc):
+                    a[i+j+1] = 2.0
+
+        for i in range(nFromPracticeInc + nPracticeInc, nTimes):
+            if np.isclose(a[i],2):
+                for j in range(nFromPracticeInc + nPracticeInc):
+                    a[i-j-1] = 2.0
+
+# Enumerate groups
+bestTimes = [[1E6, 1, 1, 1]]*nTimesToStore
+names = [p.name for p in people]
+for group1 in combinations(names,int(len(people)/2)):
+    group1 = list(group1)
+    group2 = [n for n in names if (n not in group1)]
+
+    # Generate people list from name list
+    group1People = []
+    for n in group1:
+        namesIdx = names.index(n)
+        group1People.append(people[namesIdx])
+    group2People = []
+    for n in group2:
+        namesIdx = names.index(n)
+        group2People.append(people[namesIdx])
+
+    # for each time, compute norm for each group. 
+    # If the 2 norm of group norms is less than one of best values, insert into list
+    for t in range(nTimes):
+        # Construct time vector
+        ng1 = len(group1)
+        ng2 = len(group2)
+        g1vec = np.zeros(ng1)
+        g2vec = np.zeros(ng2)
+        # Get avail value from time, name
+        for i in range(ng1):
+            # get index of day of interest
+            dayIdx = group1People[i].days.index(dayOfInterest) 
+            g1vec[i] = group1People[i].avail[dayIdx][t] 
+
+        for t2 in range(t + nPracticeInc,nTimes):
+            for i in range(ng2):
+                # get index of day of interest
+                dayIdx = group2People[i].days.index(dayOfInterest) 
+                g2vec[i] = group2People[i].avail[dayIdx][t2] 
+            
+            # Compute the norm of the vector
+            g1norm = (1.0/np.sqrt(ng1))*np.linalg.norm(g1vec)
+            g2norm = (1.0/np.sqrt(ng2))*np.linalg.norm(g2vec)
+#            print(g1vec, g2vec, g1norm, g2norm)
+            totalNorm = np.linalg.norm(np.array([g1norm, g2norm]))
+
+            # If the 2 norm of group norms is less than one of best values, insert into list
+            # This is ass code, but im in a hurry
+            for i in range(nTimesToStore):
+                if totalNorm < bestTimes[i][0]:
+                    bestTimes.append([totalNorm, times[t], group1, times[t2], group2])
+                    bestTimes.sort(key=lambda x: x[0])
+                    del bestTimes[-1]
+                    break
+
+for t in bestTimes:
+    print(t)
