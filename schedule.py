231414796ea1ae764531e4032840ac94588484f8
kyle <kcvaughn@umich.edu>
2020-08-28 12:32:58 -0400

    premliminary implementation

--- schedule.py
+++ schedule.py
@@ -7,8 +7,8 @@ from person import Person
 
 # Assumed that time increment between times is 30 min
 
-nTimesToStore = 50
-dayOfInterest = 'Monday'
+nTimesToStore = 1000
+dayOfInterest = 'Saturday'
 nPracticeInc = 6
 nToPracticeInc = 1
 nFromPracticeInc = 2
@@ -21,6 +21,14 @@ files = [
         'Jonathan.tsv',
         'Hannah.tsv',
         'Sahil.tsv',
+        'Emily.tsv',
+        'Lauren.tsv',
+        'Leah.tsv',
+        'Sam.tsv',
+        'Alanna.tsv',
+        'Griffin.tsv',
+        'Xia.tsv',
+        'Matt.tsv',
         ]
 
 module_log = logging.getLogger(__name__)
@@ -63,8 +71,10 @@ for p in people:
                     a[i-j-1] = 2.0
 
 # Enumerate groups
+module_log.info('Performing group calculations')
 bestTimes = [[1E6, 1, 1, 1]]*nTimesToStore
 names = [p.name for p in people]
+ncalc = int(0)
 for group1 in combinations(names,int(len(people)/2)):
     group1 = list(group1)
     group2 = [n for n in names if (n not in group1)]
@@ -81,7 +91,7 @@ for group1 in combinations(names,int(len(people)/2)):
 
     # for each time, compute norm for each group. 
     # If the 2 norm of group norms is less than one of best values, insert into list
-    for t in range(nTimes):
+    for t in range(nTimes-nPracticeInc):
         # Construct time vector
         ng1 = len(group1)
         ng2 = len(group2)
@@ -93,7 +103,7 @@ for group1 in combinations(names,int(len(people)/2)):
             dayIdx = group1People[i].days.index(dayOfInterest) 
             g1vec[i] = group1People[i].avail[dayIdx][t] 
 
-        for t2 in range(t + nPracticeInc,nTimes):
+        for t2 in range(t + nPracticeInc,nTimes - nPracticeInc):
             for i in range(ng2):
                 # get index of day of interest
                 dayIdx = group2People[i].days.index(dayOfInterest) 
@@ -107,6 +117,7 @@ for group1 in combinations(names,int(len(people)/2)):
 
             # If the 2 norm of group norms is less than one of best values, insert into list
             # This is ass code, but im in a hurry
+            ncalc = ncalc + 1
             for i in range(nTimesToStore):
                 if totalNorm < bestTimes[i][0]:
                     bestTimes.append([totalNorm, times[t], group1, times[t2], group2])
@@ -114,5 +125,7 @@ for group1 in combinations(names,int(len(people)/2)):
                     del bestTimes[-1]
                     break
 
+module_log.info('Done')
+print(f'Computed {ncalc} group/time combinations')
 for t in bestTimes:
     print(t)
